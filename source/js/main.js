452f5f12184d1d6a0789514e6f3e392429a47ff3
Jun <omycoke@qq.com>
2020-12-25 17:58:38 +0800

    :art: 新增顶部导航栏

--- source/js/main.js
+++ source/js/main.js
@@ -5,3 +5,28 @@ $(function(){
 $(function(){
     $('#page-banner').css('height', document.body.clientHeight/1.8);
 });
+var toggle = true;
+ 
+//导航栏按钮
+$('.btnImg').click(function() {
+	if (toggle) {
+		$('.btnImg').css("border", "1px solid #b0ccf3");
+		toggle = false
+	} else {
+		$('.btnImg').css("border", "1px solid transparent");
+		toggle = true;
+	}
+	$(".show").slideToggle(300);
+})
+ 
+//窗口大小发生改变
+$(window).resize(function() {
+	//获取窗口宽度
+	var windSize = $(window).width();
+ 
+	if (windSize > 885) {
+		$(".show").slideDown(0);
+	} else {
+		$(".show").slideUp(0);
+	}
+});
