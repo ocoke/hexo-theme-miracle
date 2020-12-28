2d92d322308de78d6d0bdc83a1072fb913839cf1
Jun <omycoke@qq.com>
2020-12-28 18:04:37 +0800

    :tada: 新增标签页,分类页,归档页与 404 错误页

---
+++ scripts/generators/pages.js
@@ -0,0 +1,52 @@
+/* global hexo */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+// generate 404 page
+if (!fs.existsSync(path.join(hexo.source_dir, '404.html'))) {
+  hexo.extend.generator.register('_404', function(locals) {
+    if (this.theme.config.page404.enable !== false) {
+      return {
+        path  : '404.html',
+        data  : locals.theme,
+        layout: '404'
+      };
+    }
+  });
+}
+
+// generate tags Page
+hexo.extend.generator.register('_tags', function(locals) {
+  if (this.theme.config.tag.enable !== false) {
+    return {
+      path  : 'tags/index.html',
+      data  : locals.theme,
+      layout: 'tags'
+    };
+  }
+});
+
+// generate categories Page
+hexo.extend.generator.register('_categories', function(locals) {
+  if (this.theme.config.category.enable !== false) {
+    return {
+      path  : 'categories/index.html',
+      data  : locals.theme,
+      layout: 'categories'
+    };
+  }
+});
+
+// generate links page
+hexo.extend.generator.register('_links', function(locals) {
+  if (this.theme.config.links.enable !== false) {
+    return {
+      path  : 'links/index.html',
+      data  : locals.theme,
+      layout: 'links'
+    };
+  }
+});
