959f33e2710c4dd95b62c4424e352804ab6991c2
Jun <omycoke@qq.com>
2021-01-09 12:49:44 +0800

    :tada: 新增代码高亮,图片懒加载并移除首页 Banner

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,5 @@
-$(function(){
-    // 将首页 Banner 设为窗口的高度
-    $('#index-banner').css('height', document.body.clientHeight);
-});
-$(function(){
-    $('#page-banner').css('height', document.body.clientHeight/1.8);
-});
+
 var toggle = true;
- 
 //导航栏按钮
 $('.btnImg').click(function() {
 	if (toggle) {
