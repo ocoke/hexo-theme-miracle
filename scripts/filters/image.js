5f87e89ad6a76b0e257953c1750949378f3c9354
Jun <omycoke@qq.com>
2021-01-12 17:56:02 +0800

    :sparkles: 增加图片灯箱

---
+++ scripts/filters/image.js
@@ -0,0 +1,61 @@
+'use strict'
+hexo.extend.filter.register(
+    'after_post_render',
+    function (data) {
+        const theme = hexo.theme.config;
+        if (theme.lazyload.enable && theme.post.fancybox.enable){
+            // 如果 FancyBox 与 Lazyload 同时开启
+            
+            data.content = data.content.replace(
+                // Match 'img' tags width the src attribute.
+                /<img([^>]*)src="([^"]*)"([^>]*)>/gim,
+                function (match, attrBegin, src, attrEnd) {
+                    // Exit if the src doesn't exists.
+                    if (!src) return match;
+
+                    return `
+                    <a data-fancybox="gallery" href="${src}">
+                        <img ${attrBegin} class="lazyload" src="/img/loading.svg" data-src="${src}" ${attrEnd} >
+                    </a>
+                    `
+                }
+            )
+        }else if (theme.lazyload.enable){
+            // 仅开启 Lazyload
+            data.content = data.content.replace(
+                // Match 'img' tags width the src attribute.
+                /<img([^>]*)src="([^"]*)"([^>]*)>/gim,
+                function (match, attrBegin, src, attrEnd) {
+                    // Exit if the src doesn't exists.
+                    if (!src) return match;
+
+                    return `<img ${attrBegin}
+                        class="lazyload"
+                        src="/img/loading.svg"
+                        data-src="${src}"
+                        ${attrEnd}
+                    >`
+                }
+            )
+        }else if (theme.post.fancybox.enable){
+            // 仅开启 Fancybox
+            data.content = data.content.replace(
+                // Match 'img' tags width the src attribute.
+                /<img([^>]*)src="([^"]*)"([^>]*)>/gim,
+                function (match, attrBegin, src, attrEnd) {
+                    // Exit if the src doesn't exists.
+                    if (!src) return match;
+
+                    return `
+                    <a data-fancybox="gallery" href="${src}">
+                        <img ${attrBegin} src="${src}" ${attrEnd}>
+                    </a>
+                    >`
+                }
+            )
+        }else{
+            return;
+        }
+    },
+    1
+);
